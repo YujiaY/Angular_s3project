d2c24d779f6a89364db71fef5746e5dec2ce0b8a
Jack <jack.yujia.yuan@gmail.com>
2018-02-20 14:55:33 +0800

    Separate two viewchild.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a description of a test',
+    '../assets/recipe-img1.jpg'),
+    new Recipe('Another Test Recipe', 'This is another description of a test',
+    '../assets/recipe-img1.jpg')
+  ];
+
+  //  public img1_source = "recipe-img1.jpg";
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Tomatoes', 10 )
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+  onIngredientAdded(addedIngredient: Ingredient) {
+    if((addedIngredient.name !== '') && (addedIngredient.amount > 0 )) {
+      this.ingredients.push(addedIngredient);
+    }
+    else alert("Please check the Name and Amount")
   }
 
 }
