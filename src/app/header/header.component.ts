ee2cdf702e4a7dba76bf1b38f288b2b2beacfd3c
Jack <jack.yujia.yuan@gmail.com>
2018-04-20 20:54:52 +0800

    Added Http approach to save&fetch data to/from FireBase.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,29 @@
 import {Component} from '@angular/core';
+import {Response} from '@angular/http';
+import {DataStorageService} from '../shared/data-storage.service';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  constructor(private dataSS:DataStorageService){}
   // @Output() featureSelected = new EventEmitter<string>();
   //
   // onSelect(feature: string) {
   //   this.featureSelected.emit(feature);
   //
   // }
+ onSaveData() {
+    this.dataSS.stroreRecipes()
+      .subscribe(
+        (response: Response)=> {
+          console.log(response)
+        }
+      );
+}
+ onFetchData() {
+    this.dataSS.getRecipes();
+
+}
 }
