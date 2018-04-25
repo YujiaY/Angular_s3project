f916cd62f99cc06ced8e97295aa362ed975c4659
Jack <jack.yujia.yuan@gmail.com>
2018-04-25 17:37:45 +0800

    Created Core Module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
-import {HomeComponent} from './home/home.component';
-import {AuthGuard} from './auth/auth-guard.service';
+import {HomeComponent} from './core/home/home.component';
 
 const appRoute: Routes = [
   // { path: '', redirectTo: '/recipes', pathMatch: 'full'},
---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,34 @@
+import {Component} from '@angular/core';
+import {Response} from '@angular/http';
+import {DataStorageService} from '../../shared/data-storage.service';
+import {AuthService} from '../../auth/auth.service';
+
+@Component({
+    selector: 'app-header',
+    styles: ['.btn-group {margin-top: 7px;}'],
+    templateUrl: './header.component.html',
+})
+export class HeaderComponent {
+  constructor(private dataSS:DataStorageService,
+              private authService: AuthService){}
+  // @Output() featureSelected = new EventEmitter<string>();
+  //
+  // onSelect(feature: string) {
+  //   this.featureSelected.emit(feature);
+  //
+  // }
+ onSaveData() {
+    this.dataSS.stroreRecipes()
+      .subscribe(
+        (response: Response)=> {
+          console.log(response)
+        }
+      );
+}
+ onFetchData() {
+    this.dataSS.getRecipes();
+  }
+  onLogout() {
+    this.authService.logout();
+  }
+}
