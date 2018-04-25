bbc58ad15a33c1f77e49f949f9acba2437d76542
Jack <jack.yujia.yuan@gmail.com>
2018-04-25 16:44:22 +0800

    Created Feature Module for Recipe and Shopping-List, added shared module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,20 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {RecipesComponent} from './recipes/recipes.component';
+
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
-import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
-import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
-import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
-import {SignupComponent} from './auth/signup/signup.component';
-import {SigninComponent} from './auth/signin/signin.component';
+import {HomeComponent} from './home/home.component';
 import {AuthGuard} from './auth/auth-guard.service';
 
 const appRoute: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  // { path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  { path: '', component: HomeComponent},
+  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
 
-  { path: 'recipes', component: RecipesComponent, children: [
-      {path: '', component: RecipeStartComponent },
-      {path: 'new', component: RecipeEditComponent, canActivate:[AuthGuard] },
-      {path: ':id', component: RecipeDetailComponent },
-      {path: ':id/edit', component: RecipeEditComponent, canActivate:[AuthGuard]  },
-    ] },
+  //Protecting Lazy Loaded Routes with canLoad:
+  // { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule', canLoad: [AuthGuard] },
+  //End of note.
   { path: 'shopping-list', component: ShoppingListComponent },
-  { path: 'recipes', component: RecipesComponent },
-  { path: 'recipes', component: RecipesComponent },
-  { path: 'signup', component: SignupComponent},
-  { path: 'signin', component: SigninComponent},
-]
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoute)],
--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,12 +1,16 @@
-import {ActivatedRoute, ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRoute, ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot} from '@angular/router';
 import {AuthService} from './auth.service';
 import {Injectable} from '@angular/core';
 
 @Injectable()
-export class AuthGuard implements CanActivate{
+export class AuthGuard implements CanActivate, CanLoad{
   constructor(private authService: AuthService){};
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.authService.isAuthenticated();
+  }
+//In this example, the AuthGuard  should implement the CanLoad interface.
+  canLoad(route: Route) {
     return this.authService.isAuthenticated();
   }
 
